0347eb10a696101aded291485df452a398b76975
iordanshdec92 <iordanhsdec92@gmail.com>
2020-11-21 15:25:36 +0200

    create validator for lab 1

---
+++ lab-1/validator.php
@@ -0,0 +1,76 @@
+<?php
+
+class Validator{
+
+    private $data;
+    private $rules;
+    private $rulesMessages;
+    private $ruleExist = false;
+    private $responseRulesMessages = [];
+
+    function __construct(){}
+
+    function validate($data, $rules = [], $rulesMessages = []){
+        foreach($rules as $key => $rule){
+            $this->ruleExist = false;
+            foreach($data as $itemKey => $item){
+                if($itemKey == $key){
+                    $this->ruleExist = true;
+                }
+            }
+
+            if($this->ruleExist == false){
+                array_push($this->responseRulesMessages, $key);
+            }
+        }
+
+        if(count($this->responseRulesMessages)){
+            return [
+                'status' => false,
+                'errors' => $this->responseRulesMessages,
+            ];
+        }
+
+        return [
+            'status' => true
+        ];
+    }
+}
+
+
+$test1 = new Validator();
+
+$data = [
+    'name' => 'Alfa'
+];
+
+$rules = [
+    'name' => [
+        'name' => 'name',
+        'required' => 1
+    ],
+    'email' => [
+        'name' => 'email',
+        'required' => 1
+    ]
+];
+
+$messages = [
+    'name' => 'Name required',
+    'email' => 'Email required',
+];
+
+$validate = $test1->validate($data, $rules, $messages);
+
+echo '<pre>';
+print_r($validate);
+echo '</pre>';
+
+if(!$validate['status']){
+    echo 'Fields Missing';
+    echo '<br>';
+    foreach($validate['errors'] as $error){
+        print_r($error);
+        echo '<br>';
+    }
+}
