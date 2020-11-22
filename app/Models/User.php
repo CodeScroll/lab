1fa9084be6d784228780e2c67d77e661f12084c2
vagrant <vagrant@lavarel-pfizer>
2020-11-22 12:26:59 +0000

    add users skills model

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function skills()
+    {
+        return $this->hasMany(Skill::class);
+    }
 }
